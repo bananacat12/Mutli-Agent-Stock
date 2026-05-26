abc7fd45ee593dddf9588f73710f75940f40546c
bananacat12 <longtyrant77777@gmail.com>
2026-05-26 11:59:47 +0700

    fix: cors allow all origins

--- my_agent/api/a2a.py
+++ my_agent/api/a2a.py
@@ -26,8 +26,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://bananacat12.github.io", "http://localhost"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
